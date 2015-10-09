08db3726458ba28f84e1b005682c9f4bf75a354c
Roderick Gadellaa <ik@rejh.nl>
2015-10-09 15:16:24 +0200

    Created project with AS

---
+++ android/com.rejh.callscreenoff/app/src/main/java/callscreenoff/rejh/com/callscreenoff/CsoActivity.java
@@ -0,0 +1,13 @@
+package callscreenoff.rejh.com.callscreenoff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CsoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cso);
+    }
+}
