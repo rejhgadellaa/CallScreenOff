931b96f231e700c0a7ad182040c65a89c6c1f34b
Roderick Gadellaa <ik@rejh.nl>
2015-10-11 09:36:04 +0200

    New icon, bit more text in activity and tweaked installer batch file

--- android/installRelease.bat
+++ android/installRelease.bat
@@ -10,8 +10,14 @@ if exist C:\android\android-sdk\tools\android.bat set androidsdk=C:\android\andr
 if exist C:\android\sdk\tools\android.bat set androidsdk=C:\android\sdk\
 
 cd %path_prj%
+
+echo Copy apk to dropbox...
+copy %cd%\app\app-release.apk D:\Desktop\Dropbox\__Static\various\apks\%name_prj%.apk
+
+echo.
+echo Install and run on device...
 %androidsdk%platform-tools\adb -d install -r %cd%\app\app-release.apk
 %androidsdk%platform-tools\adb -d shell am start %package%/.%name_act%
 
 echo.
-pause
+choice /C QYN /N /T 10 /D N /M "Press 'Q' to quit"
